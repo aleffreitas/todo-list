4ef29ca86a1c688a8938c0fe76826e5f54f811db
Alef Freitas <alef.freitas07@gmail.com>
2021-12-20 09:58:17 -0300

    Configurado local storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoForm from "./components/TodoForm";
 import Item from './components/Item';
 import List from './components/List';
 import './App.css';
 
+const SAVED_ITEMS = 'savedItems'
+
 function Todo(){
     
     const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS));
+        if (savedItems){
+            setItems(savedItems);
+        }
+    }, []);
+
+    useEffect(()=>{
+        localStorage.setItem(SAVED_ITEMS, JSON.stringify(items));
+    }, [items]);
 
     function onAddItem(text){
 
